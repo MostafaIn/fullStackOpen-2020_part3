c2d33b073310ec093f5f4bb5b6a0a65a98c9c91e
mostafaIn <mostafa2015se@gmail.com>
2020-06-06 17:28:03 +0300

    3.11 phonebook full stack

---
+++ build/precache-manifest.47c358f81a4970fd68f4ba9e3923a9af.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d19a0c5703e6f90cdf184f92f1e66b90",
+    "url": "/index.html"
+  },
+  {
+    "revision": "28f0be46c66cce238ac7",
+    "url": "/static/css/main.a940f668.chunk.css"
+  },
+  {
+    "revision": "49bdc01c5b65f8d5ccd5",
+    "url": "/static/js/2.3b9df0ff.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.3b9df0ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28f0be46c66cce238ac7",
+    "url": "/static/js/main.0f925204.chunk.js"
+  },
+  {
+    "revision": "68d5127cb03992670844",
+    "url": "/static/js/runtime-main.684b035f.js"
+  }
+]);
